7ae26b3303ed6359589e67de2c6ab0a8c8319191
Aaron Prindle <aprindle@google.com>
2025-05-07 02:47:33 +0000

    feat: add list element access to +k8s:subfield

--- staging/src/k8s.io/apimachinery/pkg/api/validate/subfield.go
+++ staging/src/k8s.io/apimachinery/pkg/api/validate/subfield.go
@@ -27,6 +27,9 @@ import (
 // nilable value.
 type GetFieldFunc[Tstruct any, Tfield any] func(*Tstruct) Tfield
 
+// MatchFn takes a pointer to an item and returns true if it matches the criteria.
+type MatchFn[T any] func(*T) bool
+
 // Subfield validates a subfield of a struct against a validator function.
 func Subfield[Tstruct any, Tfield any](ctx context.Context, op operation.Operation, fldPath *field.Path, newStruct, oldStruct *Tstruct,
 	fldName string, getField GetFieldFunc[Tstruct, Tfield], validator ValidateFunc[Tfield]) field.ErrorList {
@@ -37,5 +40,46 @@ func Subfield[Tstruct any, Tfield any](ctx context.Context, op operation.Operati
 		oldVal = getField(oldStruct)
 	}
 	errs = append(errs, validator(ctx, op, fldPath.Child(fldName), newVal, oldVal)...)
+	return errs
+}
+
+func ListMapElementByKey[TList ~[]TItem, TItem any](
+	ctx context.Context, op operation.Operation, fldPath *field.Path,
+	newList, oldList TList,
+	matches MatchFn[TItem],
+	elementValidator func(ctx context.Context, op operation.Operation, fldPath *field.Path, newObj, oldObj *TItem) field.ErrorList,
+) field.ErrorList {
+	var errs field.ErrorList
+	processedOldIndices := make(map[int]bool)
+
+	for i := range newList {
+		curI := &newList[i]
+		if !matches(curI) {
+			continue
+		}
+		var oldJ *TItem
+
+		for j := range oldList {
+			if processedOldIndices[j] {
+				continue
+			}
+
+			if matches(&oldList[j]) {
+				oldJ = &oldList[j]
+				processedOldIndices[j] = true
+				break
+			}
+		}
+		// Pass matching element present in newList and oldList to validator.
+		errs = append(errs, elementValidator(ctx, op, fldPath.Index(i), curI, oldJ)...)
+	}
+
+	for i := range oldList {
+		oldI := &oldList[i]
+		if !processedOldIndices[i] && matches(oldI) {
+			// Pass matching element present only in oldList to validator.
+			errs = append(errs, elementValidator(ctx, op, fldPath.Index(i), nil, oldI)...)
+		}
+	}
 	return errs
 }
